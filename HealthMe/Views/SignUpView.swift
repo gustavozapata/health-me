78ca1f52a52743779863945dfc173351051e5cab
Gustavo Zapata <tavordie@hotmail.com>
2020-12-30 20:16:44 +0000

    style signup view

--- HealthMe/Views/SignUpView.swift
+++ HealthMe/Views/SignUpView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 let lightGrey = Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0)
 let lightBorder = Color(red: 211/255, green: 225/255, blue: 230/255)
+let blueGray = Color(red: 143/255, green: 157/255, blue: 181/255)
 
 struct SignUpView: View {
     
@@ -28,47 +29,47 @@ struct SignUpView: View {
     var body: some View {
         ScrollView {
             VStack{
-                TextField("Full Name", text: $fullname)
-                    .padding(10)
-                    .cornerRadius(50)
+                VStack(alignment: .leading){
+                    Text("Full Name").font(.system(size: 14)).fontWeight(.semibold).foregroundColor(blueGray)
+                    TextField("John Doe", text: $fullname)
+                        .padding(10)
+                        .cornerRadius(50)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 8)
+                                .stroke(Color.gray, lineWidth: 1)
+                        )
+                        .padding(.bottom, 12)
+                    Text("Email").font(.system(size: 14)).fontWeight(.semibold).foregroundColor(blueGray)
+                    TextField("user@email.com", text: $email)
+                        .padding(10)
+                        .cornerRadius(50)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 8)
+                                .stroke(Color.gray, lineWidth: 1)
+                        )
+                        .padding(.bottom, 12)
+                    Text("Password").font(.system(size: 14)).fontWeight(.semibold).foregroundColor(blueGray)
+                    SecureField("6+ characters", text: $password)
+                        .padding(10)
+                        .cornerRadius(50)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 8)
+                                .stroke(Color.gray, lineWidth: 1)
+                        )
+                        .padding(.bottom, 20)
+                }
+                Text("Create account").fontWeight(.medium).padding(9)
+                    .font(.system(size: 16))
+                    .cornerRadius(10)
                     .overlay(
-                        RoundedRectangle(cornerRadius: 50)
-                            .stroke(Color.gray, lineWidth: 1)
-                    )
-                    .padding(.bottom, 20)
-                TextField("Telephone", text: $telephone)
-                    .padding(10)
-                    .cornerRadius(50)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 50)
-                            .stroke(Color.gray, lineWidth: 1)
-                    )
-                    .padding(.bottom, 20)
-                TextField("Email", text: $email)
-                    .padding(10)
-                    .cornerRadius(50)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 50)
-                            .stroke(Color.gray, lineWidth: 1)
-                    )
-                    .padding(.bottom, 20)
-                SecureField("Password", text: $password)
-                    .padding(10)
-                    .cornerRadius(50)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 50)
-                            .stroke(Color.gray, lineWidth: 1)
-                    )
-                    .padding(.bottom, 20)
-                Text("Sign Up").padding(10)
-                    .cornerRadius(30)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 30)
-                            .stroke(Color.blue, lineWidth: 1)
-                    )
-                
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.green, lineWidth: 2)
+                    ).padding(.bottom, 8)
+                HStack {
+                    Text("Have an account?").foregroundColor(blueGray)
+                    Text("Log in").underline().foregroundColor(.green)
+                }.font(.system(size: 13.5))
             }.padding(25)
-            
         }.navigationBarTitle("Create Account")
     }
 }
